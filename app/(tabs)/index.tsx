a043b2a4431851e6ec3bf2ac1e22bfc810bbe801
Hinata Yamaguchi <meru7374@gmail.com>
2026-06-16 11:58:36 +0900

    修正

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,28 +1,7 @@
-import { useMigrations } from 'drizzle-orm/expo-sqlite/migrator';
 import { View } from "react-native";
 import { Text } from "tamagui";
-import { db } from '../../db/client';
-import migrations from '../../drizzle/migrations';
 
 export default function App() {
-  const { success, error } = useMigrations(db, migrations);
-
-  if (error) {
-    return (
-      <View>
-        <Text>Migration error: {error.message}</Text>
-      </View>
-    );
-  }
-
-  if (!success) {
-    return (
-      <View>
-        <Text>Migration is in progress...</Text>
-      </View>
-    );
-  }
-
   return(
     <View>
         <Text>Migration is completed</Text>
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,46 @@
-import { useColorScheme } from '@/hooks/use-color-scheme'
-import config from '@/tamagui.config'
-import { TamaguiProvider } from '@tamagui/core'
-import { Stack } from 'expo-router'
+import { db } from "@/db/client";
+import migrations from "@/drizzle/migrations";
+import { useColorScheme } from '@/hooks/use-color-scheme';
+import config from '@/tamagui.config';
+import { TamaguiProvider } from '@tamagui/core';
+import { useMigrations } from "drizzle-orm/expo-sqlite/migrator";
+import { Stack } from 'expo-router';
+import { View } from "react-native";
+import { Text } from "tamagui";
 
 export const unstable_settings = { anchor: '(tabs)' }
+
+function DatabaseProvider({ children }: { children: React.ReactNode }) {
+  const { success, error } = useMigrations(db, migrations);
+
+  if (error) {
+      return (
+        <View>
+          <Text>Migration error: {error.message}</Text>
+        </View>
+      );
+    }
+  
+    if (!success) {
+      return (
+        <View>
+          <Text>Migration is in progress...</Text>
+        </View>
+      );
+    }
+
+  return <>{children}</>;
+}
 
 export default function RootLayout() {
   const colorScheme = useColorScheme()
   return (
     <TamaguiProvider config={config} defaultTheme={colorScheme ?? 'light'}>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack>
+      <DatabaseProvider>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+      </DatabaseProvider>
     </TamaguiProvider>
   )
 }
